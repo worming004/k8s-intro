72f3735f34e8ca50e9dfb9c831442c1c7753405e
Mathieu Scolas <worming@hotmail.be>
2021-12-09 02:00:25 +0100

    single file to generate new state

--- build-presentation.py
+++ build-presentation.py
@@ -1,5 +1,27 @@
-import os as os
+import os
 
-for x in os.walk('.'):
-    if './.' not in x[0]:
-        print(x)
+# na = [1,2,3]
+# for n in na:
+#     print(n)
+
+predicateAndDo = []
+
+def buildDeploy(root, _, f):
+    currentDir = os.getcwd()
+    os.chdir(os.path.join(root))
+    os.system(f)
+    os.chdir(currentDir)
+
+def fileIs(fileName):
+    return lambda _, __, file: file == fileName
+
+
+predicateAndDo.append((lambda root, _, file : fileIs('readme.md'), lambda root, _, file : os.system(f'markdown-to-slides ./{root}/{file} > ./{root}/presentation.html')))
+# predicateAndDo.append((lambda _, __, file : fileIs('builddeploy.sh'), buildDeploy))
+
+for (root, dirs, files) in os.walk("."):
+    if './.' not in root:
+        for fi in files:
+            for predicate, do in predicateAndDo:
+                if predicate(root, dirs, fi):
+                    do(root, dirs, fi)
