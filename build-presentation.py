ee2c4853ba7f6e3950dff74adf512a247c6afb1a
Mathieu Scolas <worming@hotmail.be>
2021-12-09 02:09:11 +0100

    more try

--- build-presentation.py
+++ build-presentation.py
@@ -7,8 +7,11 @@ import os
 predicateAndDo = []
 
 def buildDeploy(root, _, f):
+    rootName = root[2:]
     currentDir = os.getcwd()
-    os.chdir(os.path.join(root))
+    fp = os.path.join(os.getcwd(), rootName)
+    os.chdir(fp)
+    print(fp)
     os.system(f)
     os.chdir(currentDir)
 
@@ -16,8 +19,8 @@ def fileIs(fileName):
     return lambda _, __, file: file == fileName
 
 
-predicateAndDo.append((lambda root, _, file : fileIs('readme.md'), lambda root, _, file : os.system(f'markdown-to-slides ./{root}/{file} > ./{root}/presentation.html')))
-# predicateAndDo.append((lambda _, __, file : fileIs('builddeploy.sh'), buildDeploy))
+predicateAndDo.append((fileIs('readme.md'), lambda root, _, file : os.system(f'markdown-to-slides ./{root}/{file} > ./{root}/presentation.html')))
+predicateAndDo.append((fileIs('builddeploy.sh'), buildDeploy))
 
 for (root, dirs, files) in os.walk("."):
     if './.' not in root:
